206a0f0eecf8f23dde9e5c22398127e5fa870e82
AhmadWakeel018 <syedahmadshah018@gmail.com>
2024-07-26 10:09:06 +0500

    code push

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary:"#ffec00",
+        secondary:"#231f1e",
+        tertiary:"#FF344F",
+        bgColor:"#F7F9FD"
+      },
+      backgroundImage: {
+        'hero-bg': "url('/src/assets/hero-bg.jpg')",
+      }
+    },
+    screens: {
+      'sm': '576px',
+      'md': '960px',
+      'lg': '1366px',
+      'xl': '1366px',
+      '2xl': '1366px'
+    }
+  },
+  plugins: [],
+}
