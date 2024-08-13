e9f45eabda1621ed90e99594326584d698be9026
AhmadWakeel018 <syedahmadshah018@gmail.com>
2024-08-13 17:04:26 +0500

    Campaigns Page

--- src/index.js
+++ src/index.js
@@ -1,29 +1,37 @@
 document.addEventListener('DOMContentLoaded', function () {
     const staticHeader = document.getElementById('staticHeader');
     const stickyHeader = document.getElementById('stickyHeader');
+    const splide1El = document.querySelector('.slider-one');
+    const splide2El = document.querySelector('.slider-two');
+    const splide3El = document.querySelector('.slider-three');
+    const splide4El = document.querySelector('.slider-four');
     const scrollThreshold = 200; // Adjust this value as needed
     
-    var splide1 = new Splide('.slider-one', {
-        type: 'loop',
-        perPage: 3,
-        gap: '20px',
-        autoplay: true,
-        interval: 3000
-    });
+    function initSplide(element, className, perPageProp) {
+        if (element) {
+            console.log(`Initializing ${className}`);
+            try {
+                new Splide(element, {
+                    type: 'loop',
+                    perPage: perPageProp,
+                    gap: '20px',
+                    autoplay: true,
+                    pagination:false,
+                    interval: 3000
+                }).mount();
+                console.log(`${className} initialized successfully`);
+            } catch (error) {
+                console.error(`Error initializing ${className}:`, error);
+            }
+        } else {
+            console.warn(`Element with class ${className} not found`);
+        }
+    }
 
-    var splide2 = new Splide('.slider-two', {
-        type: 'loop',
-        perPage: 3,
-        gap: '20px',
-        autoplay: true,
-        interval: 3000
-    });
-    splide1.mount();
-    splide2.mount();
-    // splide3.mount();
-
-
-
+    initSplide(splide1El, '.slider-one',3);
+    initSplide(splide2El, '.slider-two',3);
+    initSplide(splide3El, '.slider-three',4);
+    initSplide(splide4El, '.slider-four',4);
 
     function handleScroll() {
         if (window.scrollY > scrollThreshold) {
